8a8142fd3078b90f459032e1b5b88ea800ae0a2c
RajShelfEx <rajmani.shukla@shelfexecution.com>
2024-12-20 11:34:34 +0530

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,7 +1,7 @@
 # !pip install roboflow
 
 from roboflow import Roboflow
-rf = Roboflow(api_key="4BMHBXI4YPtMpL0lEfm3")
+rf = Roboflow(api_key=<API-KEY>)
 project = rf.workspace("sample-aoipo").project("masterdataset")
 version = project.version(3)
 dataset = version.download("yolov11")
